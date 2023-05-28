ffe3d11c0c36c6841e9a6717be017d8966dc7fb4
Artem <ivann3692gmail.com>
2023-05-28 10:52:40 +0300

    V 1.2

---
+++ PP_3_1_5/src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.model.Role;
+import java.util.List;
+import java.util.NoSuchElementException;
+public interface RoleDao {
+    List<Role> findAll();
+    Role findRoleByAuthority(String authority) throws NoSuchElementException;
+
+}
